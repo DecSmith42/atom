ff5910b4becc4c7ee922150ce4551e6b8f0a1e2f
Declan Smith <dec.smith42@gmail.com>
2024-06-12 23:41:26 +1000

    Add dotnet setup step to github workflow generator

--- DecSm.Atom.GithubWorkflows/Generation/GithubWorkflowWriter.cs
+++ DecSm.Atom.GithubWorkflows/Generation/GithubWorkflowWriter.cs
@@ -8,25 +8,25 @@ public sealed class GithubWorkflowWriter(
 ) : WorkflowFileWriter<GithubWorkflowType>(fileSystem, logger)
 {
     private readonly IFileSystem _fileSystem = fileSystem;
-    
+
     protected override string FileExtension => "yml";
-    
+
     protected override int TabSize => 2;
-    
+
     protected override AbsolutePath FileLocation => _fileSystem.SolutionRoot() / ".github" / "workflows";
-    
+
     protected override void WriteWorkflow(WorkflowModel workflow)
     {
         WriteLine($"name: {workflow.Name}");
         WriteLine();
-        
+
         using (WriteSection("on:"))
         {
             var manualTrigger = workflow
                 .Triggers
                 .OfType<GithubManualTrigger>()
                 .FirstOrDefault();
-            
+
             if (manualTrigger is not null)
                 using (WriteSection("workflow_dispatch:"))
                 {
@@ -38,43 +38,43 @@ public sealed class GithubWorkflowWriter(
                                 WriteLine($"{input.Name}:");
                                 WriteLine($"  description: {input.Description}");
                                 WriteLine($"  required: {input.Required}");
-                                
+
                                 switch (input)
                                 {
                                     case GithubManualBoolInput boolInput:
-                                        
+
                                         WriteLine("type: bool");
-                                        
+
                                         if (boolInput.DefaultValue is not null)
                                             WriteLine($"default: {boolInput.DefaultValue}");
-                                        
+
                                         break;
-                                    
+
                                     case GithubManualStringInput stringInput:
-                                        
+
                                         WriteLine("type: string");
-                                        
+
                                         if (stringInput.DefaultValue is not null)
                                             WriteLine($"default: {stringInput.DefaultValue}");
-                                        
+
                                         break;
-                                    
+
                                     case GithubManualChoiceInput choiceInput:
-                                        
+
                                         WriteLine("type: string");
-                                        
+
                                         using (WriteSection("options:"))
                                         {
                                             foreach (var choice in choiceInput.Choices)
                                                 WriteLine($"- {choice}");
                                         }
-                                        
+
                                         break;
                                 }
                             }
                         }
                 }
-            
+
             foreach (var pullRequestTrigger in workflow.Triggers.OfType<GithubPullRequestTrigger>())
                 using (WriteSection("pull_request:"))
                 {
@@ -84,7 +84,7 @@ public sealed class GithubWorkflowWriter(
                             foreach (var branch in pullRequestTrigger.IncludedBranches)
                                 WriteLine($"- {branch}");
                         }
-                    
+
                     if (pullRequestTrigger.ExcludedBranches?.Count > 0)
                         using (WriteSection("branches-ignore:"))
                         {
@@ -92,7 +92,7 @@ public sealed class GithubWorkflowWriter(
                                 WriteLine($"- {branch}");
                         }
                 }
-            
+
             foreach (var pushTrigger in workflow.Triggers.OfType<GithubPushTrigger>())
                 using (WriteSection("push:"))
                 {
@@ -102,7 +102,7 @@ public sealed class GithubWorkflowWriter(
                             foreach (var branch in pushTrigger.IncludedBranches)
                                 WriteLine($"- {branch}");
                         }
-                    
+
                     if (pushTrigger.ExcludedBranches?.Count > 0)
                         using (WriteSection("branches-ignore:"))
                         {
@@ -111,9 +111,9 @@ public sealed class GithubWorkflowWriter(
                         }
                 }
         }
-        
+
         WriteLine();
-        
+
         using (WriteSection("jobs:"))
         {
             foreach (var job in workflow.Jobs)
@@ -123,7 +123,7 @@ public sealed class GithubWorkflowWriter(
             }
         }
     }
-    
+
     private void WriteJob(WorkflowModel workflow, WorkflowJobModel job)
     {
         using (WriteSection($"{job.Name}:"))
@@ -133,15 +133,15 @@ public sealed class GithubWorkflowWriter(
                 var jobRequirementNames = job.JobDependencies;
                 WriteLine($"needs: [ {string.Join(", ", jobRequirementNames)} ]");
             }
-            
+
             WriteLine("runs-on: ubuntu-latest");
-            
+
             var outputs = new List<string>();
-            
+
             foreach (var step in job.Steps.OfType<CommandWorkflowStep>())
                 outputs.AddRange(buildModel.Targets.Single(t => t.Name == step.Name)
                     .ProducedVariables);
-            
+
             if (outputs.Count > 0)
                 using (WriteSection("outputs:"))
                 {
@@ -149,7 +149,7 @@ public sealed class GithubWorkflowWriter(
                         WriteLine(
                             $"{buildDefinition.ParamDefinitions[output].Attribute.ArgName}: ${{{{ steps.{job.Name}.outputs.{buildDefinition.ParamDefinitions[output].Attribute.ArgName} }}}}");
                 }
-            
+
             using (WriteSection("steps:"))
             {
                 foreach (var step in job.Steps)
@@ -160,19 +160,31 @@ public sealed class GithubWorkflowWriter(
             }
         }
     }
-    
+
     private void WriteStep(WorkflowModel workflow, WorkflowJobModel job, IWorkflowStepModel step)
     {
         switch (step)
         {
             case CommandWorkflowStep commandStep:
-                
-                WriteLine("- name: Checkout");
-                WriteLine("  uses: actions/checkout@v2");
+
+                using (WriteSection("- name: Checkout"))
+                    WriteLine("uses: actions/checkout@v2");
+
                 WriteLine();
-                
+
+                using (WriteSection("- name: Setup .NET"))
+                {
+                    WriteLine("uses: actions/setup-dotnet@v4");
+
+                    using (WriteSection("with:"))
+                        WriteLine("dotnet-version: 8.x");
+                }
+
+
+                WriteLine();
+
                 var target = buildModel.Targets.Single(t => t.Name == commandStep.Name);
-                
+
                 if (target.ConsumedArtifacts.Count > 0)
                 {
                     if (workflow
@@ -185,7 +197,7 @@ public sealed class GithubWorkflowWriter(
                             buildModel.Targets.Single(t => t.Name == nameof(IDownloadArtifact.DownloadArtifact)),
                             [("download-artifact-name", string.Join(";", target.ConsumedArtifacts.Select(x => x.ArtifactName)))],
                             false);
-                        
+
                         WriteLine();
                     }
                     else
@@ -195,21 +207,21 @@ public sealed class GithubWorkflowWriter(
                             using (WriteSection($"- name: Download {artifact.ArtifactName}"))
                             {
                                 WriteLine("uses: actions/download-artifact@v4");
-                                
+
                                 using (WriteSection("with:"))
                                 {
                                     WriteLine($"name: {artifact.ArtifactName}");
                                     WriteLine($"path: \"{Github.PipelineArtifactDirectory}/{artifact.ArtifactName}\"");
                                 }
                             }
-                            
+
                             WriteLine();
                         }
                     }
                 }
-                
+
                 WriteCommandStep(workflow, commandStep, target, [], true);
-                
+
                 if (target.ProducedArtifacts.Count > 0)
                 {
                     if (workflow
@@ -227,22 +239,22 @@ public sealed class GithubWorkflowWriter(
                             using (WriteSection($"- name: Upload {artifact.ArtifactName}"))
                             {
                                 WriteLine("uses: actions/upload-artifact@v4");
-                                
+
                                 using (WriteSection("with:"))
                                 {
                                     WriteLine($"name: {artifact.ArtifactName}");
                                     WriteLine($"path: \"{Github.PipelinePublishDirectory}/{artifact.ArtifactName}\"");
                                 }
                             }
-                            
+
                             WriteLine();
                         }
                 }
-                
+
                 break;
         }
     }
-    
+
     private void WriteCommandStep(
         WorkflowModel workflow,
         CommandWorkflowStep commandStep,
@@ -252,20 +264,20 @@ public sealed class GithubWorkflowWriter(
     {
         var assemblyName = Assembly.GetEntryAssembly()!.GetName()
             .Name!;
-        
+
         using (WriteSection($"- name: {commandStep.Name}"))
         {
             if (includeId)
                 WriteLine($"id: {commandStep.Name}");
-            
+
             WriteLine($"run: dotnet run --project {assemblyName}/{assemblyName}.csproj {commandStep.Name} --skip --headless");
-            
+
             var env = new Dictionary<string, string>();
-            
+
             foreach (var consumedVariable in target.ConsumedVariables)
                 env[buildDefinition.ParamDefinitions[consumedVariable.VariableName].Attribute.ArgName] =
                     $"${{{{ needs.{consumedVariable.TargetName}.outputs.{buildDefinition.ParamDefinitions[consumedVariable.VariableName].Attribute.ArgName} }}}}";
-            
+
             if (target
                 .RequiredParams
                 .Select(x => buildDefinition.ParamDefinitions[x])
@@ -273,18 +285,18 @@ public sealed class GithubWorkflowWriter(
                 foreach (var injectedSecret in workflow.Options.OfType<WorkflowSecretInjection>())
                 {
                     var paramDefinition = buildDefinition.ParamDefinitions.GetValueOrDefault(injectedSecret.Param);
-                    
+
                     if (paramDefinition is not null)
                         env[paramDefinition.Attribute.ArgName] =
                             $"${{{{ secrets.{paramDefinition.Attribute.ArgName.ToUpper().Replace('-', '_')} }}}}";
                 }
-            
+
             if (env.Count > 0)
                 using (WriteSection("env:"))
                 {
                     foreach (var (key, value) in env)
                         WriteLine($"{key}: {value}");
-                    
+
                     foreach (var (key, value) in extraParams)
                         WriteLine($"{key}: {value}");
                 }
