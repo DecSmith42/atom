31101109cee8f00f8d377d552a19a9bf12567efa
Declan Smith <dec.smith42@gmail.com>
2024-06-13 23:34:41 +1000

    Re-enabled PushToNuget and added Qodana token

--- _atom/Helpers/INugetHelper.cs
+++ _atom/Helpers/INugetHelper.cs
@@ -7,9 +7,7 @@ public partial interface INugetHelper : IProcessHelper
     {
         Logger.LogInformation("Pushing package to Nuget: {PackagePath}", packagePath);
 
-        Logger.LogInformation("[Dry run] nuget push \"{PackagePath}\" --source {Feed} --api-key ***", packagePath, feed);
-
-        // await RunProcess("dotnet", $"nuget push \"{packagePath}\" --source {feed} --api-key {apiKey}");
-        // Logger.LogInformation("Package pushed");
+        await RunProcess("dotnet", $"nuget push \"{packagePath}\" --source {feed} --api-key {apiKey}");
+        Logger.LogInformation("Package pushed");
     }
 }
