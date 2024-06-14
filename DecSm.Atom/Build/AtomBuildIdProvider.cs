a277cdf180fb656eb977e707898bcfebdf28e61b
Declan Smith <dec.smith42@gmail.com>
2024-06-14 22:54:53 +1000

    Fixed AtomBuildIdProvider seconds since epoch logic

--- DecSm.Atom/Build/AtomBuildIdProvider.cs
+++ DecSm.Atom/Build/AtomBuildIdProvider.cs
@@ -4,5 +4,5 @@ internal sealed class AtomBuildIdProvider : IBuildIdProvider
 {
     private long? _buildId;
 
-    public long BuildId => _buildId ??= int.Parse($"{(DateTimeOffset.UtcNow - DateTimeOffset.UnixEpoch).Seconds}");
+    public long BuildId => _buildId ??= int.Parse($"{(DateTimeOffset.UtcNow - DateTimeOffset.UnixEpoch).TotalSeconds:0}");
 }
