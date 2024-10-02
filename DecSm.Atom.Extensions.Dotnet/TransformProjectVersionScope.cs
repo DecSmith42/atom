c2d8fa1a5d2cab653636e02dd7634bdcf932370c
Declan Smith <dec.smith42@gmail.com>
2024-10-02 04:29:42 +0000

    Fixed issue with prerelease package dependency versions

--- DecSm.Atom.Extensions.Dotnet/TransformProjectVersionScope.cs
+++ DecSm.Atom.Extensions.Dotnet/TransformProjectVersionScope.cs
@@ -4,4 +4,7 @@ public static class TransformProjectVersionScope
 {
     public static ITransformFileScope Create(AbsolutePath file, SemVer version) =>
         ITransformFileScope.Create(file, text => MsBuildUtil.SetVersionInfo(text, version));
+
+    public static ITransformMultiFileScope Create(IEnumerable<AbsolutePath> files, SemVer version) =>
+        ITransformMultiFileScope.Create(files, text => MsBuildUtil.SetVersionInfo(text, version));
 }
--- DecSm.Atom/Paths/TransformFileScope.cs
+++ DecSm.Atom/Paths/TransformFileScope.cs
@@ -61,3 +61,67 @@ internal class TransformFileScope : ITransformFileScope
         GC.SuppressFinalize(this);
     }
 }
+
+public interface ITransformMultiFileScope : IAsyncDisposable, IDisposable
+{
+    public static async Task<ITransformMultiFileScope> CreateAsync(IEnumerable<AbsolutePath> files, Func<string, string> transform)
+    {
+        var initialContents = await Task.WhenAll(files.Select(x => x.FileSystem.File.ReadAllTextAsync(x)));
+
+        var scope = new TransformMultiFileScope(files, initialContents);
+
+        await Task.WhenAll(files.Select((x, i) => x.FileSystem.File.WriteAllTextAsync(x, transform(initialContents[i]))));
+
+        return scope;
+    }
+
+    public static ITransformMultiFileScope Create(IEnumerable<AbsolutePath> files, Func<string, string> transform)
+    {
+        var initialContents = files.Select(x => x.FileSystem.File.ReadAllText(x)).ToArray();
+
+        var scope = new TransformMultiFileScope(files, initialContents);
+
+        foreach (var (file, i) in files.Select((x, i) => (x, i)))
+            file.FileSystem.File.WriteAllText(file, transform(initialContents[i]));
+
+        return scope;
+    }
+}
+
+internal class TransformMultiFileScope : ITransformMultiFileScope
+{
+    private readonly IEnumerable<AbsolutePath> _files;
+    private readonly string?[] _initialContents;
+    private bool _disposed;
+
+    internal TransformMultiFileScope(IEnumerable<AbsolutePath> files, string?[] initialContents)
+    {
+        _files = files;
+        _initialContents = initialContents;
+    }
+
+    public void Dispose()
+    {
+        if (_disposed)
+            return;
+
+        foreach (var (file, i) in _files.Select((x, i) => (x, i)))
+            file.FileSystem.File.WriteAllText(file, _initialContents[i]);
+
+        _disposed = true;
+
+        GC.SuppressFinalize(this);
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        if (_disposed)
+            return;
+
+        await Task.WhenAll(_files.Select((x, i) => x.FileSystem.File.WriteAllTextAsync(x, _initialContents[i])));
+
+        _disposed = true;
+
+        GC.SuppressFinalize(this);
+    }
+}
