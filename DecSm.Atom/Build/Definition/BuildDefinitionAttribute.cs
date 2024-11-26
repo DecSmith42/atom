25dc894d42178d6fbdf6126b7e97ef87a39805bf
Declan Smith <dec.smith42@gmail.com>
2024-11-26 23:36:06 +1000

    chore(documentation): Added documentation to some files
    Added to:
    - BuildDefinition.cs
    - BuildDefinitionAttribute.cs
    - GenerateProgramAttribute.cs
    
    Signed-off-by: Declan Smith <declan@stemlogic.co>

--- DecSm.Atom/Build/Definition/BuildDefinitionAttribute.cs
+++ DecSm.Atom/Build/Definition/BuildDefinitionAttribute.cs
@@ -1,5 +1,9 @@
 ﻿namespace DecSm.Atom.Build.Definition;
 
+/// <summary>
+///     An attribute used to mark a class as a build definition within the Atom framework.
+///     This attribute is applied to classes that define the structure and behavior of a build process.
+/// </summary>
 [PublicAPI]
 [AttributeUsage(AttributeTargets.Class)]
 public sealed class BuildDefinitionAttribute : Attribute;
