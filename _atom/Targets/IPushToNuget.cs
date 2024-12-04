bdb789020ba4dd9d3d308ac9609616adcaef99d2
Declan Smith <dec.smith42@gmail.com>
2024-12-04 22:27:42 +1000

    refactor: Remove redundant default value for default nuget feed

--- _atom/Targets/IPushToNuget.cs
+++ _atom/Targets/IPushToNuget.cs
@@ -4,7 +4,7 @@
 internal partial interface IPushToNuget : INugetHelper
 {
     [ParamDefinition("nuget-feed", "The Nuget feed to push to.", "https://api.nuget.org/v3/index.json")]
-    string NugetFeed => GetParam(() => NugetFeed) ?? "https://api.nuget.org/v3/index.json";
+    string NugetFeed => GetParam(() => NugetFeed)!;
 
     [SecretDefinition("nuget-api-key", "The API key to use to push to Nuget.")]
     string NugetApiKey => GetParam(() => NugetApiKey)!;
