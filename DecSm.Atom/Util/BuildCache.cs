11bb48720214b39b36d5623d42d56cf2b80a9c83
Declan Smith <dec.smith42@gmail.com>
2025-12-01 14:35:52 +1000

    feat(Util): add BuildCache<T> for caching build objects
    Introduced `BuildCache<T>` in the `DecSm.Atom.Util` namespace. This utility provides a generic mechanism for caching build objects using `IBuildAccessor` as keys. Features include adding, retrieving, and clearing cache entries.

---
+++ DecSm.Atom/Util/BuildCache.cs
@@ -0,0 +1,17 @@
+﻿namespace DecSm.Atom.Util;
+
+[PublicAPI]
+public sealed class BuildCache<T>
+{
+    private readonly Dictionary<IBuildAccessor, T> _cache = [];
+
+    public bool TryGetValue(IBuildAccessor key, [MaybeNullWhen(false)] out T result) =>
+        _cache.TryGetValue(key, out result);
+
+    [return: NotNullIfNotNull("value")]
+    public T Set(IBuildAccessor key, T value) =>
+        _cache[key] = value;
+
+    public void Clear() =>
+        _cache.Clear();
+}
