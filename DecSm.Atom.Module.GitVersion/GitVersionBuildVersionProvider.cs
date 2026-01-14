17596bfd23adf837f440eb8eb6472c42dad68791
Declan Smith <dec.smith42@gmail.com>
2026-01-14 21:08:20 +1000

    feat(semver): refactor SemVer to readonly struct and enhance parsing logic
    Converted `SemVer` from a class to a readonly struct to improve performance and immutability. Added new methods for parsing and TryParse with `ReadOnlySpan<char>` support. Improved null handling, default initialization, and error robustness. Updated related test cases for consistency.

--- DecSm.Atom.Module.GitVersion/GitVersionBuildVersionProvider.cs
+++ DecSm.Atom.Module.GitVersion/GitVersionBuildVersionProvider.cs
@@ -34,7 +34,7 @@ internal sealed class GitVersionBuildVersionProvider(
     {
         get
         {
-            if (field is not null)
+            if (field != default)
                 return field;
 
             var currentGitHash = processRunner
